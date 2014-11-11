207ef0ec9f0bec8bcffd4eb939e21443a017f7ce
Travis Smith <mr_travis_smith@hotmail.com>
2014-11-11 22:44:25 +0000

    database migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :projects
+
+  resources :users
+
+  resources :titles
+
+  resources :skills
+
+  resources :departments
+
+  resources :locations
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
