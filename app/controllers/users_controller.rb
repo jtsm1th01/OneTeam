9e923517a475c3178e32b3e33d631d59a760daa2
Travis Smith <mr_travis_smith@hotmail.com>
2014-11-19 00:05:55 +0000

    fixed controller params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:UserName, :UserEmail)
+      params.require(:user).permit(:user_name, :user_email)
     end
 end
