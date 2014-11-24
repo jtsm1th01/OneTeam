2e7ddf1dcbabd111b17d20a104a1bb238b9706c6
Travis Smith <mr_travis_smith@hotmail.com>
2014-11-24 21:45:12 +0000

    fixed associations for projreq show page

--- app/models/project_request.rb
+++ app/models/project_request.rb
@@ -1,8 +1,8 @@
 class ProjectRequest < ActiveRecord::Base
-  has_one :department
+  belongs_to :department
   belongs_to :location
   belongs_to :group
-  has_many :skills
+  belongs_to :skill
   belongs_to :project
   belongs_to :user
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ActiveRecord::Base
   belongs_to :users
-  belongs_to :project_requests
+  has_many :project_requests
 end
