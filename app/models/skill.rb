d0ae6262faee9c22ac8a5c59da3538db867c57fc
Travis Smith <mr_travis_smith@hotmail.com>
2014-12-02 08:04:44 +0000

    added habtm rel to users skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ActiveRecord::Base
-  belongs_to :users
+  has_and_belongs_to_many :users
   has_many :project_requests
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :skills
+  has_and_belongs_to_many :skills
   belongs_to :location
   belongs_to :department
   has_one :title
