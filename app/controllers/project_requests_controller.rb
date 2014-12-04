74fb4f8b6205d0eaf088dc4d2ee2b472327aa615
Travis Smith <mr_travis_smith@hotmail.com>
2014-12-04 22:39:39 +0000

    heroku test

--- app/controllers/project_requests_controller.rb
+++ app/controllers/project_requests_controller.rb
@@ -69,6 +69,6 @@ class ProjectRequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_request_params
-      params.require(:project_request).permit(:project_id, :employee_id, :group_id, :description)
+      params.require(:project_request).permit(:project_id, :employee_id, :group_id, :description) #test update
     end
 end
