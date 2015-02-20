ac4d510ab4de6db62e8298d290ab67f9392fb63b
Travis Smith <mr_travis_smith@hotmail.com>
2015-02-20 07:43:54 +0000

    updated styling

--- app/models/project_request.rb
+++ app/models/project_request.rb
@@ -1,5 +1,7 @@
 class ProjectRequest < ActiveRecord::Base
   validates :description, presence: true
+  validates :project_id, presence: true
+  
   has_many :skills, :through => :required_skills
   has_many :required_skills, :dependent => :destroy
   belongs_to :project
@@ -12,19 +14,5 @@ class ProjectRequest < ActiveRecord::Base
   scope :current, -> { where('end_date>?', Date.today-1) }
   scope :open, -> { where('filled=?', false) }
   scope :started, -> { where('start_date<?', Date.today+1) }
-  
-  # takes a developer parameter and returns a count of how many skills the developer has that the request asks for
-  def qualified_count(employee)
-    ps=project_request.skills
-    es=employee.skills
-    q=ps&es
-    return q.count
-  end
-  
-  # takes a developer parameter and returns a count of how many skills the developer is interested in that the request asks for
-  def interest_count
-    i=current_user.goals&project_request.skills
-    i.count
-  end
  
 end
