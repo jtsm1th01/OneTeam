f76b46e937cdc9d03ff9e6d613bdb557cab484f2
Travis Smith <mr_travis_smith@hotmail.com>
2015-04-01 16:49:04 +0000

    change review form back button route, clean up review files and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,10 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-  end
 
   def new
     @review = Review.new
     @assignment = Assignment.find(params[:assignment_id])
     @review.skill_reviews.build
     @employee = Employee.find(@assignment.employee_id)
-  end
-
-  def edit
   end
 
   def create
@@ -27,24 +16,8 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def update
-    if @review.update(review_params)
-      redirect_to @review, notice: 'Review was successfully updated.' 
-    else
-      render :edit 
-    end
-  end
-
-  def destroy
-    @review.destroy
-    redirect_to reviews_url, notice: 'Review was successfully destroyed.'
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
