6060895096ee6d6c6a237162897411cc88b9d9eb
Travis Smith <mr_travis_smith@hotmail.com>
2015-04-02 21:09:45 +0000

    change skill match to skill relevance, validate unique current/desired skills

--- app/models/current_skill.rb
+++ app/models/current_skill.rb
@@ -3,4 +3,5 @@ class CurrentSkill < ActiveRecord::Base
   belongs_to :skill
   
   validates :skill_level, presence: true
+  validates :skill_id, uniqueness: { scope: :employee_id }
 end
--- app/models/desired_skill.rb
+++ app/models/desired_skill.rb
@@ -1,4 +1,7 @@
 class DesiredSkill < ActiveRecord::Base
   belongs_to :employee
   belongs_to :skill #is this needed?
+  
+  validates :skill_id, uniqueness: { scope: :employee_id }
+  
 end
