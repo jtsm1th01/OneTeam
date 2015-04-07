db2e0b921d6f41b5203df77c81b49dca8e11d7b4
Travis Smith <mr_travis_smith@hotmail.com>
2015-04-07 22:01:29 +0000

    update review form radio button def value to reflect current skill level value

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+    
+  def default_radio_value(employee, skill, button_value)
+    current_skill = CurrentSkill.find_by(:employee => employee, :skill => skill)
+    if !current_skill.nil? && current_skill.skill_level == button_value
+      return true
+    elsif button_value == 0
+      return true
+    end
+  end
+  
 end
