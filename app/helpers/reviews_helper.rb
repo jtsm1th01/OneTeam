be97261d36fc89f4e111001ab3a76412ddf4f799
Travis Smith <mr_travis_smith@hotmail.com>
2015-04-11 08:07:24 +0000

    update review form defaults to show existing data on edit if record exists

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,16 +1,33 @@
 module ReviewsHelper
     
-  def default_radio_value(employee, skill, button_value)
-    current_skill = CurrentSkill.find_by(:employee => employee, :skill => skill)
-    if !current_skill.nil? && current_skill.skill_level == button_value
-      return true
-    elsif button_value == 0
-      return true
+  def default_skill_value(employee, skill, button_value)
+    if @review.skill_reviews.find_by(:skill => skill)
+      skill_review_level = @review.skill_reviews.find_by(:skill => skill).skill_level 
+      if skill_review_level == button_value
+        return true
+      else
+        return false
+      end
+    else
+      current_skill = CurrentSkill.find_by(:employee => employee, :skill => skill)
+      if !current_skill.nil? && current_skill.skill_level == button_value
+        return true
+      elsif button_value == 0
+        return true
+      end
     end
   end
   
   def project_duration(project_request)
     (project_request.end_date - project_request.start_date).to_i + 1
+  end
+  
+  def default_experience_value(project_request, skill)
+    if @review.skill_reviews.find_by(:skill => skill)
+      return @review.skill_reviews.find_by(:skill => skill).experience 
+    else
+      (project_request.end_date - project_request.start_date).to_i + 1
+    end
   end
   
 end
