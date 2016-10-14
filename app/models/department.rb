3ea0e3e8d02adbca40cafa2e96289f8189ea48e5
Travis Smith <mr_travis_smith@hotmail.com>
2016-10-14 19:27:55 +0000

    add department validations and display form errors

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,5 @@ class Department < ActiveRecord::Base
   has_many :employees
   has_many :groups
   has_many :project_requests, through: :groups #needed (not used?)
+  validates :department_name, presence: true, uniqueness: true
 end
